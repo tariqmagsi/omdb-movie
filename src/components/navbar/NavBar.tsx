31276a1f67fbf16977f218126e02a7ed79ee5776
Tariq Magsi <tariqhussainmagsi@Tariqs-Air.fritz.box>
2023-07-08 18:11:10 +0200

    updated logo

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -7,7 +7,9 @@ import Link from 'next/link';
 const inter = Inter({ subsets: ['latin'] })
 
 const NavBar: React.FC<NavBarProps> = () => <nav className={[styles.navbar, inter.className].join(' ')}>
-  <div className={styles.logo}>OMDB</div>
+  <div className={styles.logo}>
+    <Link href="/">OMDB</Link>
+  </div>
   <ul className={styles.navLinks}>
     <div className={styles.menu}>
       <li><Link href="/">Search</Link></li>
